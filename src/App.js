d001f7cfbe072951b4a9a62852a26722d83fee2f
prashant kumar <prashantdoa1996@gmail.com>
2023-07-25 14:17:25 +0530

    about section completed with responsive

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import About from "./components/About";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
-     <Navbar/>
-     <Home/>
+      <Navbar />
+      <Home />
+      <About />
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import { HiArrowNarrowRight } from 'react-icons/hi'
 
 const Home = () => {
   return (
-    <div name='name' className='w-full h-screen bg-[#0a192f]'>
+    <div name='home' className='w-full h-screen bg-[#0a192f]'>
 
       {/* Container */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
