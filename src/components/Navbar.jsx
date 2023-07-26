d71f135f85794987669661b803f1f3d74f2b17e7
prashant kumar <prashantdoa1996@gmail.com>
2023-07-26 14:44:16 +0530

    react-scroll initialized for mobile view

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -51,11 +51,31 @@ const Navbar = () => {
 
             {/* menu for mobile view  */}
             <ul className={!nav ? 'hidden' : ' absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
-                <li className='py-6 text-4xl'>Home</li>
-                <li className='py-6 text-4xl'>About</li>
-                <li className='py-6 text-4xl'>Skills</li>
-                <li className='py-6 text-4xl'>Work</li>
-                <li className='py-6 text-4xl'>Contact</li>
+                <li className='py-6 text-4xl'>
+                    <Link to="home" smooth={true} duration={500} >
+                        Home
+                    </Link>
+                </li>
+                <li className='py-6 text-4xl'>
+                    <Link to="about" smooth={true} duration={500} >
+                        About
+                    </Link>
+                </li>
+                <li className='py-6 text-4xl'>
+                    <Link to="skills" smooth={true} duration={500} >
+                        Skills
+                    </Link>
+                </li>
+                <li className='py-6 text-4xl'>
+                    <Link to="work" smooth={true} duration={500} >
+                        Work
+                    </Link>
+                </li>
+                <li className='py-6 text-4xl'>
+                    <Link to="contact" smooth={true} duration={500} >
+                        Contact
+                    </Link>
+                </li>
             </ul>
 
 
