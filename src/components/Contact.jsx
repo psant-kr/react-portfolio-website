fef6f035ba322eeeada1880aa2e6e1543e29a545
prashant kumar <prashantdoa1996@gmail.com>
2023-07-26 14:13:03 +0530

    getform.io used in contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
     return (
         <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
-            <form action="" className='flex flex-col max-w-[600px] w-full'>
+            <form method='POST' action="https://getform.io/f/2db08833-3c2b-4ad9-bae1-95c85e9b48f9" className='flex flex-col max-w-[600px] w-full'>
                 <div>
                     <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
                     <p className='text-gray-300 py-4'>// Submit the form below or shoot me an email - 4psant@gmail.com</p>
