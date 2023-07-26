a54955f29af1c1c1399754cdcaccbdf807569f31
prashant kumar <prashantdoa1996@gmail.com>
2023-07-26 15:23:51 +0530

    scroll added to view work button with arrow

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { HiArrowNarrowRight } from 'react-icons/hi'
+import { Link } from 'react-scroll'
 
 const Home = () => {
   return (
@@ -20,12 +21,22 @@ const Home = () => {
           focused on building responsive front-end web applications.
         </p>
         <div>
-          <button className='text-white group border-2 px-6  py-3 my-2 flex items-center hover:bg-pink-600'>
+          {/* <button className='text-white group border-2 px-6  py-3 my-2 flex items-center hover:bg-pink-600'>
             View Work
             <span className='group-hover:rotate-90 duration-300'>
               <HiArrowNarrowRight className='ml-3' />
             </span>
-          </button>
+          </button> */}
+
+          {/* scroll added to view work button  */}
+          <Link to="work" smooth={true} duration={500} >
+            <button className='text-white group border-2 px-6  py-3 my-2 flex items-center hover:bg-pink-600'>
+              View Work
+              <span className='group-hover:rotate-90 duration-300'>
+                <HiArrowNarrowRight className='ml-3' />
+              </span>
+            </button>
+          </Link>
         </div>
       </div>
     </div>
