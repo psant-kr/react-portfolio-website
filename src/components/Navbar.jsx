8118076e5d9be3bf61540dda974c2080af6ca8ed
prashant kumar <prashantdoa1996@gmail.com>
2023-07-26 14:35:25 +0530

    react-scroll installed and initialized for dexktop view

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa'
 import { HiOutlineMail } from 'react-icons/hi'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
 import Logo from '../assets/logo.png'
+import { Link } from 'react-scroll'
 
 const Navbar = () => {
     const [nav, setNav] = useState(false);
@@ -15,13 +16,32 @@ const Navbar = () => {
             </div>
 
             {/* menu for desktop view */}
-
             <ul className='hidden md:flex'>
-                <li>Home</li>
-                <li>About</li>
-                <li>Skills</li>
-                <li>Work</li>
-                <li>Contact</li>
+                <li>
+                    <Link to="home" smooth={true} duration={500} >
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link to="about" smooth={true} duration={500} >
+                        About
+                    </Link>
+                </li>
+                <li>
+                    <Link to="skills" smooth={true} duration={500} >
+                        Skills
+                    </Link>
+                </li>
+                <li>
+                    <Link to="work" smooth={true} duration={500} >
+                        Work
+                    </Link>
+                </li>
+                <li>
+                    <Link to="contact" smooth={true} duration={500} >
+                        Contact
+                    </Link>
+                </li>
             </ul>
 
             {/* nav icons  */}
@@ -30,7 +50,6 @@ const Navbar = () => {
             </div>
 
             {/* menu for mobile view  */}
-
             <ul className={!nav ? 'hidden' : ' absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
                 <li className='py-6 text-4xl'>Home</li>
                 <li className='py-6 text-4xl'>About</li>
